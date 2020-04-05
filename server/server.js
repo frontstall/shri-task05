eea814afeafd930e3836b2023274a847e93dd5ea
frontstall <deepspiral@yandex.ru>
2020-04-05 20:17:56 +0300

    fix minor bug

--- server/server.js
+++ server/server.js
@@ -30,7 +30,7 @@ const initServer = (port) => {
   if (process.env.NODE_ENV === 'production') {
     app.use(Express.static(path.resolve(__dirname, '..', 'build')));
 
-    app.get('*', (req, res) => {
+    app.get('/', (req, res) => {
       res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
     });
   }
